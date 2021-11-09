949b41d2c629b87f8a214dcd9037bc5ba1eb335a
unknown <you@example.com>
2021-11-09 15:36:04 +0200

    added hello, world

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const names = ["nkosiphendule", "sophie", "happy", "mduduzi", "ntombela", "lucky", "sithemiso", "khumbulani", "mmapaseka", "wandile", "selina", "andisiwe", "vusumuzi", "kagisho", "precious"];
-const display = document.getElementById("display");
 
-for (let i = 0; i < names.length; i++) {
-    display.innerHTML += "<hr>" + names[i]
-}
 
