156107a9f338c3228350910af336513cb00df23c
unknown <you@example.com>
2021-11-10 08:45:46 +0200

    added alert('World')

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ alert('Hello'); alert('World');
 alert(3 +
     1
     + 2)
+    
     // This comment occupies a line of its own
 alert('Hello');
 alert('World'); // This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+alert('Hello');
+alert('World');
